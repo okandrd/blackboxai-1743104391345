c156bde24e1704d71508a41c5177a9e508b20458
Okan Durdu <46085224+okandrd@users.noreply.github.com>
2025-03-27 23:16:42 +0300

    Add admin-panel/vite.config.ts 1743106601883

--- admin-panel/vite.config.ts
+++ admin-panel/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [react({
-    jsxRuntime: 'classic'
-  })],
+  plugins: [
+    react({
+      jsxRuntime: 'classic'
+    }),
+    nodePolyfills({
+      include: ['crypto', 'stream', 'util', 'buffer']
+    })
+  ],
   server: {
     port: 3000,
     open: true
