f914104044ead911f4f95857ce1415c491257a01
Okan Durdu <46085224+okandrd@users.noreply.github.com>
2025-03-27 23:05:44 +0300

    Add admin-panel/src/index.tsx 1743105944705

--- admin-panel/src/index.tsx
+++ admin-panel/src/index.tsx
@@ -1,15 +1,8 @@
+import { jsx as _jsx } from "react/jsx-runtime";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(_jsx(React.StrictMode, { children: _jsx(App, {}) }));
